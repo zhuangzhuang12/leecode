a7fbcd86afb7f98b3a2906b4c20503155fa149e2
lebron.guo@bondee.com <lebron.guo@bondee.com>
2024-09-09 14:50:10 +0800

    update: 01

---
+++ day1/买卖股票的最佳时机.go
@@ -0,0 +1,15 @@
+package day1
+
+func maxProfit(prices []int) int {
+	dp := make([][2]int, len(prices))
+	for i := 0; i < len(prices); i++ {
+		if i == 0 {
+			dp[i][0] = 0
+			dp[i][1] = -prices[i]
+		} else {
+			dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i])
+			dp[i][1] = max(dp[i-1][1], -prices[i])
+		}
+	}
+	return dp[len(prices)-1][0]
+}
